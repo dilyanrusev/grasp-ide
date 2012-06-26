fddf326b0991ffcbbbd2a677568252f92761577d
Dilyan Rusev <dilyanrusev@gmail.com>
2012-06-27 00:03:22 +0100

    multipage editor has 2 pages; created dedicated source code editor

---
+++ src/uk/ac/standrews/grasp/ide/editors/GraspTextEditor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package uk.ac.standrews.grasp.ide.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+/**
+ * @author Dilyan Rusev
+ *
+ */
+public class GraspTextEditor extends TextEditor {
+
+	/**
+	 * 
+	 */
+	public GraspTextEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
